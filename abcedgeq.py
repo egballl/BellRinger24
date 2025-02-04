925ae9f619a859a4382dbfe52114c275eb512a0d
egballl <148280195+egballl@users.noreply.github.com>
2025-02-04 15:03:50 -0800

    Update abcedgeq.py
    updated to keep unicode a-z

--- abcedgeq.py
+++ abcedgeq.py
@@ -1,4 +1,4 @@
 e = int(input("Shift ")) 
 x = 'Baez Knows!'
-s = ''.join((chr(ord(_) + e) for _ in x))
+s = ''.join((chr(((ord(_) - 65 + e) % 26) + 65) if _.isupper() else chr(((ord(_) - 97 + e) % 26) + 97) if _.islower() else _ for _ in x))
 print(s)
